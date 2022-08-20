7ebe7bbb7a9a985f02ba812f5c381083a0d6276c
johngerome <johngerome@gmail.com>
2022-08-20 13:34:53 +0800

    first commit

---
+++ apps/api/database/seeders/User.ts
@@ -0,0 +1,21 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'api/App/Models/User'
+
+export default class extends BaseSeeder {
+  public async run() {
+    await User.createMany([
+      {
+        first_name: 'Super',
+        last_name: 'Admin',
+        email: 'admin@rxcp.com',
+        password: 'admin',
+      },
+      {
+        first_name: 'John Gerome',
+        last_name: 'Baldonado',
+        email: 'player@rxcp.com',
+        password: 'player',
+      },
+    ])
+  }
+}
